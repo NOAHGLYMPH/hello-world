751df754ce53b0f72d9ed88743f0bd83903cea31
NOAHGLYMPH <nglymph@gmail.com>
2017-01-10 12:57:48 -0500

    Add files via upload

---
+++ calculator app.py
@@ -0,0 +1,51 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+# calculator 1 
+
+welcome = input("This app converts fahrenheit to celsius:")
+
+ 
+Fahrenheit = input("Enter temp in fahrenheit:")
+Celsius = input("Enter temp in celsius:")
+
+Fahrenheit = float(temp_fahrenheit)
+Celsius = float(temp_celsius)
+
+C = (Fº-32)x5/9
+
+round(Celsius)
+print("C", Celsius)
+
+# calculator 2
+
+welcome = input("This app calculates the area of a trapezoid:")
+
+height_trapezoid = input("Enter the height of the trapezoid")
+topbase_length = input("Enter the top base length")
+bottombase_length = input ("Enter the bottom base length")
+
+height =float(height_trapezoid)
+topbase =float(topbase_length)
+bottombase =float(bottombase_length)
+
+A = a_b/2(height)
+
+print("A", Area)
+
+# calculator 3
+
+welcome = input("This app converts Kilometers to Miles:")
+
+Kilometers = input("Enter value:")
+Miles = input("Enter value:")
+
+Kilometers = float(distance_kilometers)
+Miles = float(distance_miles)
+
+M = x+x/2+x/10+x/100
+
+round(Miles)
+print("C", Celsius)
