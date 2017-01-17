99444db08d01e25d58bd9dd10c475eccb133f53a
NOAHGLYMPH <nglymph@gmail.com>
2017-01-17 11:45:21 -0500

    Add files via upload

---
+++ Quiz phython .py
@@ -0,0 +1,245 @@
+# Trivia Quiz Stuff
+
+print("This is a quiz that tests your knowledge on the solar system")
+
+name = input('Enter Your name: ')
+
+print('\nHi there' , name + ' are you ready to take this quiz?\n')
+
+print(' I will ask you 10 questions and you will get 3 choices for each questions ')
+print(' Select the right choice by pressing A, B, or C ')
+
+print("--------------------------------------------------------------------")
+
+
+# set quiz score to 0
+
+score = 0
+score = int(score) # convert 0 into a number so we can add scores
+
+#question 1
+
+print('Question 1: What is the 9th planet from the sun')
+print("A. Neptune")
+print("B. There is no 9th planet")
+print("C. Pluto")
+print("D. Sedna")
+print("E. Niburu")
+print('')
+
+Q1answer = 'B' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="B" or Q1response == 'b':
+    print('Wow, you are a smart human,' + Q1response + ' is correct')
+    score = score +1
+
+else:
+    print('you are wrong!')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 2
+
+print('Question 2: How far is the earth from the sun?')
+print("A. 90 million miles")
+print("B. 253 million miles")
+print("C. 578 million miles")
+print("D. 93 million miles")
+print("E. 94 million miles")
+print('')
+
+Q1answer = 'D' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="D" or Q1response == 'd':
+    print('how did you know? You must be the smartest member of your species,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('you are wrong!')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 3
+
+print('Question 3: How many times has NASA sent people to the moon ')
+print("A. 20  ")
+print("B. 5 ")
+print("C. 6 ")
+print("D. 8 ")
+print("E. 1 ")
+print('')
+
+Q1answer = 'C' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="C" or Q1response == 'c':
+    print('Oh this was unexpected,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('hahaha you are wrong!')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 4
+
+print('Question 4: What is the largest asteroid in the asteroid belt')
+print("A. Hygiea")
+print("B. Vesta")
+print("C. Pallas")
+print("D. Pluto")
+print("E. Ceres")
+print('')
+
+Q1answer = 'E' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="E" or Q1response == 'e':
+    print('There is no way you knew this,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('hahaha you were wrong!')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 5
+
+print('Question 5: What is the farthest object in our solar system?')
+print("A. 2012 VP113 ")
+print("B. V774104")
+print("C. Sedna")
+print("D. Eris")
+print("E. 2014 FE72")
+print('')
+
+Q1answer = 'A' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="A" or Q1response == 'a':
+    print('Almost got you there human,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('hehehe nope')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 6
+
+print('Question 6: How many comets have been found in the solar system?')
+print("A. 1337 ")
+print("B. 4000")
+print("C. 1 million")
+print("D. 500")
+print("E. 0")
+print('')
+
+Q1answer = 'B' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="B" or Q1response == 'b':
+    print('whoa,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('hehehe nope')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 7
+
+print('Question 7: How far is the moon from the earth?')
+print("A. 100,000 miles")
+print("B. 158,000 miles")
+print("C. 346,800 miles")
+print("D. 238,900 miles")
+print("E. 900,832 miles")
+print('')
+
+Q1answer = 'D' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="D" or Q1response == 'd':
+    print('whoa how did you...,' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('hah')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 8
+
+print('Question 8: Which planets have rings')
+print("A. Saturn, Jupiter ")
+print("B. Saturn, Neptune")
+print("C. Saturn, Jupiter, Neptune, Pluto")
+print("D. Saturn, Jupiter,Neptune, Uranus")
+print("E. Earth")
+print('')
+
+Q1answer = 'D' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="D" or Q1response == 'd':
+    print('hmmm... ur cheatin' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('LOL')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 9
+
+print('Question 9: Highest measured wind speeds at Neptune?')
+print("A. 45mph ")
+print("B. 2000mph")
+print("C. 566mph")
+print("D. 350mph")
+print("E. 1500mph")
+print('')
+
+Q1answer = 'E' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="E" or Q1response == 'e':
+    print('GOTTEN HEMIL' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print(' as if')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+#question 10
+
+print('Question 10: How long is a day on saturn?')
+print("A. 9h 56m ")
+print("B. 10h 42m")
+print("C. 17h 14m")
+print("D. 16h 6m")
+print("E. 24 hours")
+print('')
+
+Q1answer = 'B' #right answer to question 1
+Q1response = input('Your answer: ')
+
+if Q1response =="B" or Q1response == 'b':
+    print('GOTTEN HEMIL' + Q1response + ' is correct')
+    score = score +2
+
+else:
+    print('as if')
+
+print('Your current score is ' + str(score) + ' out of 10')
+
+
+
+
+    
+
+
